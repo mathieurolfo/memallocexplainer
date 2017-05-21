31d87d271bcdf89cbd8aa9f7486266f50d012346
Mathieu Rolfo <rolfom01@gmail.com>
2017-05-20 17:40:40 -0700

    basic layout defined, adding logic now

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 window.onload = function() {
 
+	var cargo = [2, 3, 4];
+	var cars = [2, 4, 3];
+	var cargoLeft = cargo.length;
+	var currentCargo = cargo[0];
+	var numCars = cars.length;
+	var currentCar = 1;
 	window.initialGame = {
-		blocks: [2, 3, 4],
-		cars: [2, 4, 3]
-	}
+		cargo: cargo,
+		cars: cars,
+		numCars: numCars,
+		
+	};
 
-	window.numClicks = 0;
+	
+
+	var remainingCapacity = [2, 4, 3];
+
+	window.currentState = {
+		cargoLeft: cargoLeft,
+		currentCar: currentCar,
+		remainingCapacity: remainingCapacity,
+		currentCargo: currentCargo
+	};
+
 
 	$(init);
-
-
 }
 
 function init() {
-	$('#currentCarRegion').draggable();
-	console.log("now draggable")
+	$('#cargo-display-header').html("Cargo left to load: " + window.currentState.cargoLeft);
+	$('#cargo-current-size').html("Current cargo size: " + window.currentState.currentCargo);
+	$('#car-display-header').html("Current Car: " 
+		+ window.currentState.currentCar + " of " + window.initialGame.numCars);
+
+	refresh();
 }
 
-function buttonClicked() {
+function refresh() {
+	var capacity = window.currentState.remainingCapacity[window.currentState.currentCar];
+	var maxCapacity = window.initialGame.cars[window.currentState.currentCar];
+	$('#car-capacity').html("Remaining capacity: " + capacity + " of " + maxCapacity);
+}
+
+function leftClicked() {
+	console.log("left clicked");
 	window.numClicks += 1;
 	document.getElementById("counter").innerHTML = window.numClicks;
+}
 
+function rightClicked() {
+	console.log("right clicked");
+}
+
+function loadClicked() {
+	console.log("load clicked");
 }
 
 
