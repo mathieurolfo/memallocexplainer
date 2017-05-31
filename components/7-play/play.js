9de26b258f37b4a8294d8c0832550a64e38b412f
sunmilee <sunmilee0330@gmail.com>
2017-05-31 13:14:50 -0700

    fixed initial errors

--- components/7-play/play.js
+++ components/7-play/play.js
@@ -27,7 +27,7 @@ window.onload = function() {
 function initializeParametersSeg() {
   var cargo = [null, 8, 2, 4, 10, 7, 6, 15, "none!"];
     var cars = [null, 4, 8, 16, 10, 12, 4, 2, 8, 4, 6];
-    var car4 = [null, 4, 2, 4 4];
+    var car4 = [null, 4, 2, 4, 4];
     var car8 = [null, 6, 8, 8];
     var car12 = [null, 10, 12];
     var car16 = [null, 16];
@@ -114,10 +114,10 @@ function initWholeTrainGraphicSeg() {
 
 function redrawCargoSeg() {
     var currLeft = window.segfreeleftOffset;
-    for (var j = 1; j<window.segfreeinitialGame.cars.length; j++) {
+    for (var j = 1; j<window.segfreeInitialGame.cars.length; j++) {
         var loadedCargo = "cargo" + j;
-        var carWidth = window.segfreeinitialGame.cars[j]*25
-        var cargoWidth = (window.segfreeinitialGame.cars[j]-window.segfreeCurrentState.remainingCapacity[j])*25;
+        var carWidth = window.segfreeInitialGame.cars[j]*25
+        var cargoWidth = (window.segfreeInitialGame.cars[j]-window.segfreeCurrentState.remainingCapacity[j])*25;
         window[loadedCargo] = new fabric.Rect({top: 30, left: currLeft, width: cargoWidth, height: 20, fill: 'red'});
         window.segfreetraincanvas.add(window[loadedCargo]);
         currLeft += carWidth + 10;
@@ -128,19 +128,19 @@ function refreshSeg() {
 
 
     var capacity = window.segfreeCurrentState.remainingCapacity[window.segfreeCurrentState.currentCar];
-    var maxCapacity = window.segfreeinitialGame.cars[window.segfreeCurrentState.currentCar];
+    var maxCapacity = window.segfreeInitialGame.cars[window.segfreeCurrentState.currentCar];
     $('#seg-free-car-capacity').html("Remaining capacity: " + capacity + " of " + maxCapacity);
 
     $('#seg-free-cargo-display-header').html("Cargo loads remaining: " + window.segfreeCurrentState.cargoLeft);
-    $('#seg-free-current-cargo-size').html("Current cargo:"); //" size: " + window.initialGame.cargo[window.currentState.currentCargoIndex]);
+    $('#seg-free-current-cargo-size').html("Current cargo:"); //" size: " + window.InitialGame.cargo[window.currentState.currentCargoIndex]);
 
     $('#seg-free-car-display-header').html("Current Car: "
-        + window.segfreeCurrentState.currentCar + " of " + window.segfreeinitialGame.numCars);
+        + window.segfreeCurrentState.currentCar + " of " + window.segfreeInitialGame.numCars);
 
     $('#seg-free-clicks').html(window.segfreeCurrentState.clicks);
-    $('#seg-free-utilization').html(window.segfreeCurrentState.utilization + " out of " + window.segfreeinitialGame.totalSpace)
+    $('#seg-free-utilization').html(window.segfreeCurrentState.utilization + " out of " + window.segfreeInitialGame.totalSpace)
 
-    // if (capacity >= window.segfreeinitialGame.cargo[window.segfreecurrentState.currentCargoIndex]) {
+    // if (capacity >= window.segfreeInitialGame.cargo[window.segfreecurrentState.currentCargoIndex]) {
     //   $('#seg-free-left-button').attr("disabled", true);
     //   $('#seg-free-right-button').attr("disabled", true);
     //   $('#seg-free-message-box').html("The cargo fits! Place it here!");
@@ -165,11 +165,11 @@ It would have fit on the first car, but we filled that with smaller cargos. Scro
 }
 
 function updateCargoBoxSeg() {
-    var cargoSize = window.segfreeinitialGame.cargo[window.segfreeCurrentState.currentCargoIndex]
+    var cargoSize = window.segfreeInitialGame.cargo[window.segfreeCurrentState.currentCargoIndex]
     cargoSize *= 30
     $('#seg-free-current-cargo-box').css("width", cargoSize);
     console.log(cargoSize);
-    $('#seg-free-current-cargo-box').html(window.segfreeinitialGame.cargo[window.segfreeCurrentState.currentCargoIndex]);
+    $('#seg-free-current-cargo-box').html(window.segfreeInitialGame.cargo[window.segfreeCurrentState.currentCargoIndex]);
     
     if (window.segfreeCurrentState.cargoLeft === 3) {
     $('#seg-free-cargo2').css("display", "block");
@@ -183,7 +183,7 @@ function updateCargoBoxSeg() {
 }
 
 function updateTrainCarSeg() {
-    var trainSize = window.segfreeinitialGame.cars[window.segfreeCurrentState.currentCar];
+    var trainSize = window.segfreeInitialGame.cars[window.segfreeCurrentState.currentCar];
     var remainingSpace = window.segfreeCurrentState.remainingCapacity[window.segfreeCurrentState.currentCar];
     var filledSpace = trainSize - remainingSpace
 
@@ -199,7 +199,7 @@ function updateTrainCarSeg() {
     window.segfreewheel2.set({left: wheel2Position});
     window.segfreecarcanvas.renderAll();
 
-    if (window.segfreeCurrentState.currentCar === window.segfreeinitialGame.numCars) {
+    if (window.segfreeCurrentState.currentCar === window.segfreeInitialGame.numCars) {
       $('#seg-free-right-button').addClass("disabled");
     } else if (window.segfreeCurrentState.currentCar === 1){
       $('#seg-free-left-button').addClass("disabled");
@@ -231,7 +231,7 @@ function leftClickedSeg() {
 function rightClickedSeg() {
     $('#seg-free-message-box').html("");
 
-    if (window.segfreeCurrentState.currentCar < window.segfreeinitialGame.numCars) {
+    if (window.segfreeCurrentState.currentCar < window.segfreeInitialGame.numCars) {
         window.segfreeCurrentState.currentCar += 1;
         window.segfreeCurrentState.clicks += 1;
         refreshSeg();
@@ -241,10 +241,10 @@ function rightClickedSeg() {
 }
 
 function loadClickedSeg() {
-    if (window.segfreeinitialGame.cargo[window.segfreeCurrentState.currentCargoIndex] <= window.segfreeCurrentState.remainingCapacity[window.segfreecurrentState.currentCar]) {
+    if (window.segfreeInitialGame.cargo[window.segfreeCurrentState.currentCargoIndex] <= window.segfreeCurrentState.remainingCapacity[window.segfreecurrentState.currentCar]) {
         //load the cargo
-        window.segfreeCurrentState.remainingCapacity[window.segfreeCurrentState.currentCar] -= window.segfreeinitialGame.cargo[window.segfreecurrentState.currentCargoIndex]
-        window.segfreeCurrentState.utilization += window.segfreeinitialGame.cargo[window.segfreeCurrentState.currentCargoIndex]
+        window.segfreeCurrentState.remainingCapacity[window.segfreeCurrentState.currentCar] -= window.segfreeInitialGame.cargo[window.segfreecurrentState.currentCargoIndex]
+        window.segfreeCurrentState.utilization += window.segfreeInitialGame.cargo[window.segfreeCurrentState.currentCargoIndex]
         window.segfreeCurrentState.currentCargoIndex += 1
         window.segfreeCurrentState.cargoLeft -= 1
         window.segfreeCurrentState.currentCar = 1;
