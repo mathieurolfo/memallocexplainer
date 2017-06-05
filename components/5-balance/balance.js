27d768b51b7dd181f7ab47e8ae8850c9b7194331
Zhengshan Shi <kittyshi@ccrma.stanford.edu>
2017-06-05 01:17:01 -0700

    5-balance

--- components/5-balance/balance.js
+++ components/5-balance/balance.js
@@ -1,17 +1,47 @@
 "use strict";
 
-window.addEventListener("load", function() {
-  var button1 = document.getElementById("button-1");
 
-  var button1WasClicked = false;
+balanceInit();
 
-  button1.addEventListener("click", function() {
-    button1WasClicked = !button1WasClicked
-    if (button1WasClicked) {
-      button1.setAttribute("class", "btn btn-warning");
-    } else {
-      button1.setAttribute("class", "btn btn-danger");
-    }
-  });
-});
+function textNode(input) {
+  var textDiv = document.createElement('div');
+  textDiv.setAttribute('class', 'inline-text')
+  textDiv.textContent = input
+  return textDiv
+}
 
+function balanceInit() {
+  var container = document.getElementById('balance-sheet')
+
+  var imageDiv = document.createElement("div");
+  var imgObj = document.createElement("img");
+  imgObj.setAttribute("src", "figures/compare-firstfit.png");
+  imgObj.setAttribute("height", "145px");
+  imageDiv.appendChild(imgObj);
+  imageDiv.appendChild(textNode('Cost: 8'));
+  imageDiv.appendChild(document.createElement('br'));
+  imageDiv.appendChild(textNode('First Fit'));
+  imageDiv.appendChild(document.createElement('br'));
+
+  imgObj = document.createElement("img");
+  imgObj.setAttribute("src", "figures/compare-bestfit.png");
+  imgObj.setAttribute("height", "145px");
+  imageDiv.appendChild(imgObj);
+  imageDiv.appendChild(textNode('Cost: 12'));
+  imageDiv.appendChild(document.createElement('br'));
+  imageDiv.appendChild(textNode(' Best Fit'));
+  imageDiv.appendChild(document.createElement('br'));
+
+  // text
+  var textDiv = document.createElement('div');
+  textDiv.setAttribute('class', 'balance-text')
+  textDiv.innerHTML = "In first fit, we have low cost but more wasted space.<br>"
+  textDiv.innerHTML += "In best fit, we have high cost but less wasted space.<br>"
+  textDiv.innerHTML += "Because for the first fit, we are able to place a piece of cargo more quickly, but we’re not careful about where we place it."
+  textDiv.innerHTML += "On the other hand, for the best fit, we spend more time place each place of cargo, but leaves more room for place more future pieces of cargo."
+  textDiv.innerHTML += "However, neither strategy is ideal."
+
+  imageDiv.appendChild(textDiv)
+  container.appendChild(imageDiv);
+
+}
