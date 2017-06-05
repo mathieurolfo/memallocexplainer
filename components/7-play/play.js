4525cf32f51d813b18f3873a68de8ea78a41803e
Zhengshan Shi <kittyshi@ccrma.stanford.edu>
2017-06-05 03:52:22 -0700

    changing the figure url

--- components/7-play/play.js
+++ components/7-play/play.js
@@ -399,6 +399,7 @@ function initFullTrain() {
             left:0,
             top: 20,
         })
+        obj.scaleToHeight(40);
         window.segfreetraincanvas.add(obj);
     });
     window.segfreeleftOffset = 60;
